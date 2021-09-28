d2a4c05ea7e687106f51f8f55ddefd6dda3d132d
nu-n <mizukitty57@gmail.com>
2021-09-28 17:52:14 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,17 +5,17 @@ class MessagesController < ApplicationController
    end
  
    def create
-     @room = Room.find(params[:room_id])
-     @message = @room.messages.new(message_params)
-     if @message.save
-       redirect_to room_messages_path(@room)
-     else
-       render :index
-     end
-   end
- 
- 
-   private
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
+  end
+  
+private
  
    def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
